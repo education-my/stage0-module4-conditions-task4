25538abd922e8ccd2f8caed5e4cad85a93f9a6ef
education-my <education.mykhailo@gmail.com>
2022-09-17 00:45:20 +0300

    Module 4 - Task 4

--- src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
+++ src/main/java/school/mjc/stage0/conditions/task4/MaxNumberPrinter.java
@@ -2,5 +2,16 @@ package school.mjc.stage0.conditions.task4;
 
 public class MaxNumberPrinter {
     public void printGreatest(int first, int second, int third) {
+        int greatest;
+
+        if ((first >= second && first >= third)) {
+            greatest = first;
+        } else if (second >= first && second >= third) {
+            greatest = second;
+        } else {
+            greatest = third;
+        }
+
+        System.out.println(greatest);
     }
 }
